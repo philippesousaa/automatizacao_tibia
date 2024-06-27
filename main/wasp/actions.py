20536a4910a7698c3a15ecdaebf5b1593efd8553
Philippe Sousa <philipp3sousa@gmail.com>
2024-06-27 11:51:36 -0300

    ajustes

--- main/wasp/actions.py
+++ main/wasp/actions.py
@@ -1,43 +1,59 @@
 import constants
 import pyautogui as pg
+import keyboard as k
 
 
-battle_image = 'images/region_battle.png'
-target_image = 'images/reg_target.png'
-monster_image = 'images/monster_dead.png'
-hole_image = 'images/holy_down.png'
-anchor2_image = 'images/anchor_floor_2.png'
-anchor3_image = 'images/anchor_floor_3.png'
+battle_image = "images/region_battle.png"
+target_image = "images/reg_target.png"
+monster_image = "images/monster_dead.png"
+hole_image = "images/holy_down.png"
+anchor2_image = "images/anchor_floor_2.png"
+anchor3_image = "images/anchor_floor_3.png"
+
 
 def hole_down(should_down):
     if should_down:
         try:
+            print(f'Entando no hole_down...')
             box = pg.locateOnScreen(hole_image, confidence=0.8)
-            if box:
-                x, y = pg.center(box)
-                pg.moveTo(x, y)
-                pg.click()
-                pg.sleep(5)
+            x, y = pg.center(box)
+            pg.moveTo(x, y )
+            pg.click(button='left')
+            print(f'Saindo do hole_down...')
+            return box
         except Exception as e:
-            print('error hole down')
+            print(f'Saindo do hole_down sem buraco...')
+            return None
 
-def hole_up(img_anchor, plus_x, plus_y, should_up):
+def hole_up(should_up, img_anchor, plus_x, plus_y):
     if should_up:
-        try:
+        try:       
+            print(f'Entrado no hole_up... ')
             box = pg.locateOnScreen(img_anchor, confidence=0.8)
-            if box:
-                x, y = pg.center(box)
-                pg.moveTo(x + plus_x, y + plus_y)
-                pg.press('F4')
-                pg.click()
+            x, y = pg.center(box)
+            pg.moveTo(x + plus_x, y + plus_y)
+            pg.press("F4")
+            pg.click(button='left')
+            print(f'Saindo do hole_up com buraco....')
+            return box
         except Exception as e:
-            print('error hole up')
-
+            print(f'Saindo do hole_up sem buraco...')
+            return None
 
 def check_battle():
     try:
-        print(pg.locateOnScreen(battle_image, region=constants.REGION_BATTLE)) 
         return pg.locateOnScreen(battle_image, region=constants.REGION_BATTLE)
     except Exception as e:
-        print(f'Bicho na battle')
+        print(f"Bicho na battle")
+        pg.sleep(1)
         return None
+
+def check_status(name, delay, x, y, rgb, button_name):
+    print(f"checando {name} ....")
+    pg.sleep(delay)
+    if pg.pixelMatchesColor(x, y, rgb):
+        pg.press(button_name)
+
+def eat_food():
+    pg.press('F6')
+    print('comendo comida')
--- main/wasp/main.py
+++ main/wasp/main.py
@@ -7,102 +7,101 @@ import constants
 import json
 
 
-battle_image = 'images/region_battle.png'
-target_image = 'images/reg_target.png'
-monster_image = 'images/monster_dead.png'
-hole_image = 'images/holy_down.png'
-anchor2_image = 'images/anchor_floor_2.png'
-anchor3_image = 'images/anchor_floor_3.png'
+battle_image = "images/region_battle.png"
+target_image = "images/reg_target.png"
+monster_image = "images/monster_dead.png"
+hole_image = "images/holy_down.png"
+anchor2_image = "images/anchor_floor_2.png"
+anchor3_image = "images/anchor_floor_3.png"
 
 
 def is_attacking():
     try:
-        print('atacando')
-        return pg.locateOnScreen(target_image, region=constants.REGION_BATTLE, confidence=0.7)
+        print("atacando")
+        return pg.locateOnScreen(
+            target_image, region=constants.REGION_BATTLE, confidence=0.7
+        )
     except Exception as e:
-        print(f'Erro ao localizar a imagem is_attacking: {e}')
         return None
 
+
 def kill_monster():
-    print("actions.check_battle começando kill monster")
+    print("Entrando no kill_monster...")
     while actions.check_battle() is None:
-        print("entrou no while")
-        pg.press('space')
+        pg.press("space")
         while is_attacking() is not None:
-            print('esperando o bicho morrer')
-        print('procurando bicho')
+            print("esperando o bicho morrer")
+    print("Encerrando no kill_monster...")
+
 
 def get_loot():
     try:
-        loot = list(pg.locateAllOnScreen(monster_image, confidence=0.9, region=constants.REGION_LOOT))
-        if not loot:
-            print('Sem loot encontrado.')
-            return
-        print(f'Encontrado {len(loot)} monstros')
-        for box in loot:
-            x, y = pg.center(box)
+        print("Entrando no get_loot...")
+        loot = pg.locateAllOnScreen(
+            monster_image, confidence=0.9, region=constants.REGION_LOOT
+        )
+        for i in loot:
+            x, y = pg.center(i)
             pg.moveTo(x, y)
-            pg.click()
-            pg.sleep(0.5)
-            print('Clique realizado no loot')
+            pg.click(button="left")
+        print("Encerrando no get_loot com bicho morto ao lado...")
     except Exception as e:
-        print(f'Erro ao procurar loot: {e}')
+        print("Encerrando no get_loot sem bichos proximos...")
+        return None
 
-def go_to_flag(paths):
+def go_to_flag(path, wait):
+    print(f'entrando GO_TO_FLAG com param: {path} e {wait}...')
     try:
-        for item in paths:
-            path = item['path']
-            wait = item['wait']
-            
-            print(f'Tentando ir para: {path}')
-            while True:
-                flag = pg.locateOnScreen(path, confidence=0.8, region=constants.REGION_MAP)
-                if flag:
-                    x, y = pg.center(flag)
-                    pg.moveTo(x, y)
-                    pg.click()
-                    pg.sleep(wait)
-                    print(f'Sucesso ao ir para: {path}')
-                    while check_player_position() is None:
-                        pg.sleep(1)
-                    break  # Sai do loop interno pois chegou no path correto
-                else:
-                    print(f'Não encontrou: {path}')
-        
-        print('Concluídas todas as flags.')
-        return True
-    
+        flag = pg.locateOnScreen(path, region=constants.REGION_MAP, confidence=0.8)
+        x, y = pg.center(flag)
+        pg.moveTo(x, y)
+        pg.click(button="left")
+        pg.sleep(wait)  
+        print('Encerrando GO_TO_FLAG...')  
+        return flag
     except Exception as e:
-        print(f'Erro ao procurar a flag: {e}')
-        return False
+        return None
+
 
 def check_player_position():
     try:
-        return pg.locateOnScreen('images/point_player.png', confidence=0.8, region=constants.REGION_MAP)
+        print(f'Entrando no check_player_position')
+        check =  pg.locateOnScreen(
+            "images/point_player.png", confidence=0.8, region=constants.REGION_MAP
+        )
+        print(f'Encerrando no check_player_position achando imagem no minimapa...') 
+        return check
     except Exception as e:
-        print(f'Erro ao verificar a posição do jogador: {e}')
+        print(f'Encerrando no check_player_position não achando imagem no minimapa...') 
         return None
 
 def run():
-    try:
-        with open(f'{constants.FOLDER_NAME}/infos.json', 'r') as file:
-            data = json.load(file)
-        
-        for item in data:
-            kill_monster()
-            get_loot()
-            
-            try:
-                paths = [item]  # Passando o item como lista para go_to_flag
-                if not go_to_flag(paths):
-                    print(f'Não foi possível encontrar nenhum dos caminhos para o item: {item}')
-            
-            except Exception as e:
-                print(f'Erro ao executar go_to_flag para o item: {item}. Erro: {e}')
-                continue
-    
-    except Exception as e:
-        print(f'Erro durante a execução do script: {e}')
+    print(f'iniciando run()...') 
+    with open(f"{constants.FOLDER_NAME}/infos.json", "r") as file:
+        data = json.loads(file.read())
+    print(f'json: {data}...') 
+    for item in data:
+        kill_monster()
+        pg.sleep(1)
+        get_loot()
+        print(item["path"]+ ' SEM CHECK...')
+        go_to_flag(item["path"], item["wait"])
+        while check_player_position() != None:
+            kill_monster()  
+            pg.sleep(1)
+            get_loot()       
+            print(item["path"]+ ' COM CHECK...')
+            go_to_flag(item["path"], item["wait"])
+        actions.eat_food()
+        actions.hole_down(item['down_hole'])
+        actions.hole_up(item['up_hole'], f'{constants.anchor2_image}', 430, 0)
+        actions.hole_up(item['up_hole'], f'{constants.anchor3_image}', 130, 130)
+
 
 # Início da execução do script
-run()
+
+k.wait("h")
+while True: 
+    run()
+# check_status("mana", 1, 988, 31, (0, 63, 141), 'F3')
+# check_status("vida", 1, 838, 32, (35, 35, 35), 'F3')
